a8792c38022689e3b97c81c6d6bee1d567f667e2
Piyushnmj <piyushnmj@yahoo.com>
2022-11-18 19:36:17 +0530

    Welcome

---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Delegates and Events");
+            Console.ReadLine();
+        }
+    }
+}
